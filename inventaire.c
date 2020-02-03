80e03065c5dabd7ab8c9ef004278bef23303bafd
Bouketin <projetafter.steven@gmail.com>
2020-02-03 16:41:09 +0100

    Update inventaire.c

--- inventaire.c
+++ inventaire.c
@@ -8,21 +8,20 @@ struct Objet{
 };
 typedef struct Objet item;
 
+void viewInventaire(item * inventaire){
+	for (int i=0; i<5; i++){
+		printf ("%s ", inventaire[i].nom);
+		printf ("prix %d\n",inventaire[i].prix);	
+	}
+};
+
 int main(){
-	
-	
 	
 	//création inventaire joueur
 	int tab [5];
 	item inventaireJoueur [5] = {{"chacal", 2}, {"gilbert de compagnie", 67}};
-		
-	for (int i=0; i<5; i++){
-		printf ("%s ", inventaireJoueur[i].nom);
-		printf ("prix %d\n",inventaireJoueur[i].prix);
-		
-	}
 	
+	viewInventaire(&inventaireJoueur[0]);
 	
-	
-	
+return 0;
 }
